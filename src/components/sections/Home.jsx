51d1b1f5938354432d56e778cc496f6bba7a0255
Olstertecn11 <otzunund@miumg.edu.gt>
2023-10-19 11:53:30 -0600

    agregando seccion en inicio

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -12,6 +12,28 @@ const Home = () => {
 					bioquímicas, coprourológicas, hormonales y de enfermedades infecciosas.</p>
 				<button className='btn btn-0'>Conocer Más</button>
 			</div>
+			<div className="row p-4" >
+				<div className="row sub-row" >
+					<div className="col-md-3 mx-auto">
+						<div className="card-body">
+							<img className='img-1' src="https://labdiagnostico.com/wp-content/uploads/2021/01/CovidTest-scaled.jpg" alt="" />
+							<h2 className='text-center text-primary t-1'>Servicios</h2>
+						</div>
+					</div>
+					<div className="col-md-3 mx-auto">
+						<div className="card-body">
+							<img className='img-1' src="https://labdiagnostico.com/wp-content/uploads/2021/01/CovidTest-scaled.jpg" alt="" />
+							<h2 className='text-center text-primary t-1'>Consulta</h2>
+						</div>
+					</div>
+					<div className="col-md-3 mx-auto">
+						<div className="card-body">
+							<img className='img-1' src="https://labdiagnostico.com/wp-content/uploads/2021/01/CovidTest-scaled.jpg" alt="" />
+							<h2 className='text-center text-primary t-1'>Ubicación</h2>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
