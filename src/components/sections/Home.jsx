57cc77051243c6d178eee32c0ccb9d9e00cb3a36
Olstertecn11 <otzunund@miumg.edu.gt>
2023-10-22 12:58:44 -0600

    service section

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,41 +1,71 @@
 
 import './styles/Home.css';
+import services_icon from './../icons/Inmunolab/health.png';
+import health_icon from './../icons/Inmunolab/consul.png';
+import location_icon from './../icons/Inmunolab/location.png';
+import background from './../icons/Inmunolab/wave-haikei2.png';
+import test_tube from './../icons/Inmunolab/test_tube.png';
 
 const Home = () => {
-	return (
-		<div>
-			<div className="row">
-				<img className='image' src="https://laboratoriosedulab.com/wp-content/uploads/2020/06/Edulab_servicios_Inmunologia.jpg" alt="" />
-				<h1 className='title'>INMUNOLAB</h1>
-				<p className='p-0'>Somos un laboratorio de análisis clínico Nivel IV, especializado en el área de inmunología, puntualmente damos el soporte
-					necesario en el diagnóstico de enfermedades inmunes y autoinmunes, contando también con todo el menú de pruebas
-					bioquímicas, coprourológicas, hormonales y de enfermedades infecciosas.</p>
-				<button className='btn btn-0'>Conocer Más</button>
-			</div>
-			<div className="row p-4" >
-				<div className="row sub-row" >
-					<div className="col-md-3 mx-auto">
-						<div className="card-body">
-							<img className='img-1' src="https://labdiagnostico.com/wp-content/uploads/2021/01/CovidTest-scaled.jpg" alt="" />
-							<h2 className='text-center text-primary t-1'>Servicios</h2>
-						</div>
-					</div>
-					<div className="col-md-3 mx-auto">
-						<div className="card-body">
-							<img className='img-1' src="https://labdiagnostico.com/wp-content/uploads/2021/01/CovidTest-scaled.jpg" alt="" />
-							<h2 className='text-center text-primary t-1'>Consulta</h2>
-						</div>
-					</div>
-					<div className="col-md-3 mx-auto">
-						<div className="card-body">
-							<img className='img-1' src="https://labdiagnostico.com/wp-content/uploads/2021/01/CovidTest-scaled.jpg" alt="" />
-							<h2 className='text-center text-primary t-1'>Ubicación</h2>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
-}
+  return (
+    <div>
+      <div className="row">
+        <img className='image' src="https://laboratoriosedulab.com/wp-content/uploads/2020/06/Edulab_servicios_Inmunologia.jpg" alt="" />
+        <h1 className='title'>INMUNOLAB</h1>
+        <p className='p-0'>Somos un laboratorio de análisis clínico Nivel IV, especializado en el área de inmunología, puntualmente damos el soporte
+          necesario en el diagnóstico de enfermedades inmunes y autoinmunes, contando también con todo el menú de pruebas
+          bioquímicas, coprourológicas, hormonales y de enfermedades infecciosas.</p>
+        <button className='btn btn-0'>Conocer Más</button>
+      </div>
+      <div className="row sub-row" >
+        <div className="col-md-3 mx-auto">
+          <div className="card-body">
+            <img className='img-1' src={services_icon} alt="" />
+            <h2 className='text-center text-primary t-1'>Servicios</h2>
+          </div>
+        </div>
+        <div className="col-md-3 mx-auto">
+          <div className="card-body">
+            <img className='img-1' src={health_icon} alt="" />
+            <h2 className='text-center text-primary t-1'>Consulta</h2>
+          </div>
+        </div>
+        <div className="col-md-3 mx-auto">
+          <div className="card-body">
+            <img className='img-1' src={location_icon} alt="" />
+            <h2 className='text-center text-primary t-1'>Ubicación</h2>
+          </div>
+        </div>
+      </div>
+      <div className="row services">
+        <div className='col-md-6 nosotros mx-auto'>
+          <h2 className='text-primary text-center'>Nosotros</h2>
+          <br />
+          <p className='p-1 text-center mx-auto'>
+            Por más de 44 años hemos dado un servicio seguro, confiable y de calidad, siendo históricamente un laboratorio de alta
+            referencia.
+          </p>
+          <hr />
+          <p className='p-1 tex-center mx-auto'>
+            Inmunolab cuenta con una amplia experiencia a lo largo de los años, en metodología y tecnología
+            avanzada necesaria para realizar pruebas de diagnóstico especializada, así mismo como exámenes de
+            rutina general
+          </p>
+        </div>
+        <div className="col-md-4">
+          <img src={test_tube} className='test-tube' />
+        </div>
+        <img src={background} className="img-background mx-auto" />
+      </div >
+    </div >
 
+  );
+}
 export default Home;
+
+
+
+
+
+
+
--- src/components/widgets/Nav.jsx
+++ src/components/widgets/Nav.jsx
@@ -4,51 +4,51 @@ import './styles/Nav.css';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
-	return (
-		<div>
-			<nav className="navbar navbar-expand-lg bg-light navbar-light navbar-0">
-				<div className="container-fluid">
-					<p className="navbar-brand text-center mt-3 text-white nav-text">502+59621085 |
-						<a className='text-white nav-text' > info@inmunolab.gt</a>
-					</p>
-					<div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-						<ul className="navbar-nav">
-							<li className="nav-item">
-								<p className='mt-3 text-white nav-text'>4a Av. 13-67 zona 9, Ciudad de Guatemala</p>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</nav>
-			<nav className="navbar navbar-expand-lg bg-light navbar-light navbar-1">
-				<div className="container-fluid">
-					<a className="navbar-brand"><img src={icon} width={190} /></a>
-					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-						<span className="navbar-toggler-icon"></span>
-					</button>
-					<div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-						<ul className="navbar-nav">
-							<li className="nav-item">
-								<Link className="nav-link active" to={'/'} >Inicio</Link>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">Nosotros</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">Servicios</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">Galería</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">Contacto</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</nav>
-		</div>
-	);
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-light navbar-light navbar-0">
+        <div className="container-fluid">
+          <p className="navbar-brand text-center mt-3 text-white nav-text">502+23626887 |
+            <a className='text-white nav-text' > info@inmunolab.gt</a>
+          </p>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <p className='mt-3 text-white nav-text'>4a Av. 13-67 zona 9, Ciudad de Guatemala</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <nav className="navbar navbar-expand-lg bg-light navbar-light navbar-1">
+        <div className="container-fluid">
+          <a className="navbar-brand"><img src={icon} width={190} /></a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link active" to={'/'} >Inicio</Link>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Nosotros</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Servicios</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Galería</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contacto</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Nav;
