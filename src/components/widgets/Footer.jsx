17e8f6e7cc6e8e088a566846e9cae397c9ed80ee
Olstertecn11 <otzunund@miumg.edu.gt>
2023-11-06 11:58:12 -0600

    home responsive

--- src/components/widgets/Footer.jsx
+++ src/components/widgets/Footer.jsx
@@ -14,45 +14,45 @@ import React from 'react';
 const Footer = () => {
 
 
-  const handleClick = (link) => {
-    window.location.href = link;
-  }
+	const handleClick = (link) => {
+		window.location.href = link;
+	}
 
-  return (
-    <footer className="row footer" style={{ paddingTop: '5%' }}>
-      <div className="col-md-5 mt-3">
-        <h2 className='footer-title'>
-          Inmunolab
-          <img src={icon} className="icon" />
-        </h2>
-        <div className="items items-0">
-          <p className="footer-subtext"> <img src={location} style={{ width: '1.4vw' }} /> 4a Av. 13-67 zona 9, Ciudad de Guatemala</p>
-          <p className="footer-subtext"> <img src={phone_icon} style={{ width: '1.4vw' }} /> +502 23626887</p>
-        </div>
-      </div>
-      <div className="col-md-3 links">
-        <h2 className='mt-4 footer-text'>Enlaces</h2>
-        <div className="items">
-          <li className='li'>Nosotros</li>
-          <li className='li'>Servicios</li>
-          <li className='li'>Contactanos</li>
-        </div>
-      </div>
-      <div className="col-md-3 network">
-        <h2 className='text-center footer-text mt-4'>Síguenos</h2>
-        <div className="items">
-          <li className='text-center li' onClick={() => handleClick("https://www.facebook.com/inmunolabgt")}> <img src={face} style={{ width: '1.8vw' }} /> Facebook</li>
-          <li className='text-center li' onClick={() => handleClick("https://instagram.com/inmunolabguatemala?igshid=MzRlODBiNWFlZA==")}><img src={insta} style={{ width: '1.8vw' }} /> Instagram</li>
-          <li className='text-center li' onClick={() => handleClick("https://www.linkedin.com/company/inmunolab/")}>
-            <img src={linke} style={{ width: '1.4vw', marginRight: 10 }} alt="" />
-            Linkedin
-          </li>
-        </div>
-      </div>
-      <div className='row row-margin'>
-      </div>
-    </footer >
-  );
+	return (
+		<footer className="row footer" style={{ paddingTop: '5%' }}>
+			<div className="col-md-5 mt-3">
+				<h2 className='footer-title'>
+					Inmunolab
+					<img src={icon} className="icon" />
+				</h2>
+				<div className="items items-0">
+					<p className="footer-subtext"> <img src={location} style={{ width: '1.4vw' }} /> 4a Av. 13-67 zona 9, Ciudad de Guatemala</p>
+					<p className="footer-subtext"> <img src={phone_icon} style={{ width: '1.4vw' }} /> +502 23626887</p>
+				</div>
+			</div>
+			<div className="col-md-3 links">
+				<h2 className='mt-4 footer-text'>Enlaces</h2>
+				<div className="items">
+					<li className='li'>Nosotros</li>
+					<li className='li'>Servicios</li>
+					<li className='li'>Contactanos</li>
+				</div>
+			</div>
+			<div className="col-md-3 network">
+				<h2 className='text-center footer-text mt-4'>Síguenos</h2>
+				<div className="items">
+					<li className='text-center li' onClick={() => handleClick("https://www.facebook.com/inmunolabgt")}> <img src={face} style={{ width: '1.8vw' }} /> Facebook</li>
+					<li className='text-center li' onClick={() => handleClick("https://instagram.com/inmunolabguatemala?igshid=MzRlODBiNWFlZA==")}><img src={insta} style={{ width: '1.8vw' }} /> Instagram</li>
+					<li className='text-center li' onClick={() => handleClick("https://www.linkedin.com/company/inmunolab/")}>
+						<img src={linke} style={{ width: '1.4vw', marginRight: 10 }} className="last" />
+						Linkedin
+					</li>
+				</div>
+			</div>
+			<div className='row row-margin'>
+			</div>
+		</footer >
+	);
 };
 
 export default Footer;
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 dns.setDefaultResultOrder('verbatim');
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: { port: 3000, }
+	plugins: [react()],
+	server: { port: 5174, }
 });
