c4d267fc4d8a800674c79c2ae0acfbc7e6724a14
Oliver Tzunun <otzunund@miumg.edu.gt>
2023-12-12 20:02:00 -0600

    fixing zone

--- src/components/widgets/Footer.jsx
+++ src/components/widgets/Footer.jsx
@@ -32,7 +32,7 @@ const Footer = () => {
         <div className="items items-0">
           <Link to='https://maps.app.goo.gl/FeM64bLmfyNrFVai8' className="footer-subtext">
             <FaMapMarker className='mr-2' />
-            4A Avenida 13-67 Cdad. de Guatemala
+            4A Avenida 13-67 Zona 9, Guatemala
           </Link>
           <Link className="footer-subtext">
             <FaCalendarAlt className='mr-2' />
