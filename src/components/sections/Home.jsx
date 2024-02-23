9b57585e9d96e837589d6da41b5eac8ebf6da4dd
Oliver Tzunun <otzunund@miumg.edu.gt>
2024-02-22 22:34:05 -0600

    fix services row sizze

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -42,31 +42,6 @@ const Home = () => {
       </div>
 
       <hr />
-      <h2 className='card-title text-center info-title'>Información</h2>
-      <hr />
-      <div className="row" >
-        <div id="carouselExampleControls" className="carousel slide info-slide" data-ride="carousel" style={{ marginLeft: '30vw' }}>
-          <div className="carousel-inner" >
-            <div className="carousel-item active" style={{ background: 'none' }}>
-              <img className="d-block " src={info1} alt="First slide" style={{ width: '40vw' }} />
-            </div>
-            <div className="carousel-item" style={{ background: 'none', }}>
-              <img className="d-block " src={info2} alt="Second slide" style={{ width: '40vw' }} />
-            </div>
-          </div>
-          <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="sr-only">Previous</span>
-          </a>
-          <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="sr-only">Next</span>
-          </a>
-        </div>
-
-      </div>
-
-      <hr />
 
       <div className="row services animate__animated animate__fadeInLeft">
         <div className='col-md-6 nosotros mx-auto'>
